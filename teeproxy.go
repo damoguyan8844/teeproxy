aed1a97bb3914ccf31295335eadf6151b61098b4
Andrew Wardle <andy.wardle@googlemail.com>
2014-10-27 08:12:11 +0000

    Modified to use reverse proxy

--- teeproxy.go
+++ teeproxy.go
@@ -8,7 +8,8 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
-	"runtime"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -19,49 +20,9 @@ var (
 	debug            = flag.Bool("debug", false, "more logging, showing ignored output")
 )
 
-type handler struct {
-	Target      string
-	Alternative string
-}
-
-func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	req1, req2 := DuplicateRequest(req)
-	go func() {
-		defer func() {
-			if r := recover(); r != nil && *debug {
-				fmt.Println("Recovered in f", r)
-			}
-		}()
-		client_tcp_conn, _ := net.DialTimeout("tcp", h.Alternative, time.Duration(1*time.Second)) // Open new TCP connection to the server
-		client_http_conn := httputil.NewClientConn(client_tcp_conn, nil)                          // Start a new HTTP connection on it
-		client_http_conn.Write(req1)                                                              // Pass on the request
-		client_http_conn.Read(req1)                                                               // Read back the reply
-		client_http_conn.Close()                                                                  // Close the connection to the server
-	}()
-	defer func() {
-		if r := recover(); r != nil && *debug {
-			fmt.Println("Recovered in f", r)
-		}
-	}()
-
-	client_tcp_conn, _ := net.DialTimeout("tcp", h.Target, time.Duration(3*time.Second)) // Open new TCP connection to the server
-	client_http_conn := httputil.NewClientConn(client_tcp_conn, nil)                     // Start a new HTTP connection on it
-	client_http_conn.Write(req2)                                                         // Pass on the request
-	resp, _ := client_http_conn.Read(req2)                                               // Read back the reply
-	resp.Write(w)                                                                        // Write the reply to the original connection
-	client_http_conn.Close()                                                             // Close the connection to the server
-}
-
-func main() {
-	flag.Parse()
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	local, _ := net.Listen("tcp", *listen)
-	h := handler{
-		Target:      *targetProduction,
-		Alternative: *altTarget,
-	}
-	http.Serve(local, h)
+type Hosts struct {
+	Target      url.URL
+	Alternative url.URL
 }
 
 type nopCloser struct {
@@ -70,11 +31,73 @@ type nopCloser struct {
 
 func (nopCloser) Close() error { return nil }
 
-func DuplicateRequest(request *http.Request) (request1 *http.Request, request2 *http.Request) {
+type myTransport struct {
+}
+
+var hosts Hosts
+
+func (t *myTransport) RoundTrip(request *http.Request) (*http.Response, error) {
+	response, err := http.DefaultTransport.RoundTrip(request)
+
+	if response != nil {
+		r, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			// copying the response body did not work
+			return nil, err
+		}
+
+		fmt.Printf("[<A Response>][<%v>]\n", string(r))
+	}
+
+	return response, err
+}
+
+func teeDirector(req *http.Request) {
+	fmt.Printf("[<Request>][<%v>]\n", req)
+	req2 := duplicateRequest(req)
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil && *debug {
+				fmt.Println("Recovered in f", r)
+			}
+		}()
+		client_tcp_conn, _ := net.DialTimeout("tcp", hosts.Alternative.Host, time.Duration(1*time.Second))
+		client_http_conn := httputil.NewClientConn(client_tcp_conn, nil)
+		client_http_conn.Write(req2)
+		resp, _ := client_http_conn.Read(req2)
+		r, _ := httputil.DumpResponse(resp, true)
+		fmt.Printf("[<B Response>][<%v>]\n", string(r))
+
+		client_http_conn.Close()
+	}()
+
+	targetQuery := hosts.Target.RawQuery
+	req.URL.Scheme = hosts.Target.Scheme
+	req.URL.Host = hosts.Target.Host
+	req.URL.Path = singleJoiningSlash(hosts.Target.Path, req.URL.Path)
+	if targetQuery == "" || req.URL.RawQuery == "" {
+		req.URL.RawQuery = targetQuery + req.URL.RawQuery
+	} else {
+		req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func duplicateRequest(request *http.Request) (request1 *http.Request) {
 	b1 := new(bytes.Buffer)
-	b2 := new(bytes.Buffer)
-	w := io.MultiWriter(b1, b2)
-	io.Copy(w, request.Body)
+	io.Copy(b1, request.Body)
 	defer request.Body.Close()
 	request1 = &http.Request{
 		Method:        request.Method,
@@ -87,16 +110,29 @@ func DuplicateRequest(request *http.Request) (request1 *http.Request, request2 *
 		Host:          request.Host,
 		ContentLength: request.ContentLength,
 	}
-	request2 = &http.Request{
-		Method:        request.Method,
-		URL:           request.URL,
-		Proto:         "HTTP/1.1",
-		ProtoMajor:    1,
-		ProtoMinor:    1,
-		Header:        request.Header,
-		Body:          nopCloser{b2},
-		Host:          request.Host,
-		ContentLength: request.ContentLength,
-	}
 	return
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	u, _ := url.Parse(*targetProduction)
+	proxy := httputil.NewSingleHostReverseProxy(u)
+	proxy.Transport = &myTransport{}
+	proxy.Director = teeDirector
+
+	proxy.ServeHTTP(w, r)
+}
+
+func main() {
+	flag.Parse()
+
+	target, _ := url.Parse(*targetProduction)
+	alt, _ := url.Parse(*altTarget)
+
+	hosts = Hosts{
+		Target:      *target,
+		Alternative: *alt,
+	}
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(*listen, nil)
 }
