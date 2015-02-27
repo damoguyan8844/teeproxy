b4e1fef77556d7872c968f2002440f157b41ff40
Andrew Wardle <andy.wardle@googlemail.com>
2015-02-27 09:54:07 +0000

    Fixing issue with querystring not copying for replicated request

--- teeproxy.go
+++ teeproxy.go
@@ -17,8 +17,8 @@ import (
 
 var (
 	listen           = flag.String("l", ":8888", "port to accept requests")
-	targetProduction = flag.String("a", "localhost:8080", "where production traffic goes. http://localhost:8080/production")
-	altTarget        = flag.String("b", "localhost:8081", "where testing traffic goes. response are skipped. http://localhost:8081/test")
+	targetProduction = flag.String("a", "http://localhost:8080", "where production traffic goes. http://localhost:8080/production")
+	altTarget        = flag.String("b", "http://localhost:8081", "where testing traffic goes. response are skipped. http://localhost:8081/test")
 	debug            = flag.Bool("debug", false, "more logging, showing ignored output")
 )
 
@@ -33,43 +33,10 @@ var proxy *httputil.ReverseProxy
 
 type TimeoutTransport struct {
 	http.Transport
-	RoundTripTimeout time.Duration
 }
 
-type respAndErr struct {
-	resp *http.Response
-	err  error
-}
-
-type netTimeoutError struct {
-	error
-}
-
-func (ne netTimeoutError) Timeout() bool { return true }
-
-// If you don't set RoundTrip on TimeoutTransport, this will always timeout at 0
 func (t *TimeoutTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	timeout := time.After(t.RoundTripTimeout)
-	resp := make(chan respAndErr, 1)
-
-	go func() {
-		r, e := t.Transport.RoundTrip(req)
-
-		resp <- respAndErr{
-			resp: r,
-			err:  e,
-		}
-	}()
-
-	select {
-	case <-timeout: // A round trip timeout has occurred.
-		t.Transport.CancelRequest(req)
-		return nil, netTimeoutError{
-			error: fmt.Errorf("timed out after %s", t.RoundTripTimeout),
-		}
-	case r := <-resp: // Success!
-		return r.resp, r.err
-	}
+	return t.Transport.RoundTrip(req)
 }
 
 func teeDirector(req *http.Request) {
@@ -137,9 +104,10 @@ func duplicateRequest(request *http.Request) (request1 *http.Request) {
 	request2 := &http.Request{
 		Method: request.Method,
 		URL: &url.URL{
-			Scheme: hosts.Alternative.Scheme,
-			Host:   hosts.Alternative.Host,
-			Path:   singleJoiningSlash(hosts.Alternative.Path, request.URL.Path),
+			Scheme:   hosts.Alternative.Scheme,
+			Host:     hosts.Alternative.Host,
+			Path:     singleJoiningSlash(hosts.Alternative.Path, request.URL.Path),
+			RawQuery: request.URL.RawQuery,
 		},
 		Proto:         "HTTP/1.1",
 		ProtoMajor:    1,
@@ -172,9 +140,7 @@ func main() {
 
 	u, _ := url.Parse(*targetProduction)
 	proxy = httputil.NewSingleHostReverseProxy(u)
-	proxy.Transport = &TimeoutTransport{
-		RoundTripTimeout: time.Second * 60,
-	}
+	proxy.Transport = &TimeoutTransport{}
 	proxy.Director = teeDirector
 
 	http.HandleFunc("/", handler)
